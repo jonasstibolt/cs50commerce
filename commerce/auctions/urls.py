04829c790ea002717ec6d2238012997bdb7f171c
jonasstibolt <jonasholmgaard@gmail.com>
2024-09-15 10:48:50 +0200

    update form

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
@@ -8,5 +7,4 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path('create/', views.createListing, name='createListing'), #hvor skal man have / efter create?
     path("register", views.register, name="register"),
-    path('adminpage/', admin.site.urls),
 ]
